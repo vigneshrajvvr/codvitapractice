1584b2dc25a2354716deacc3f8eb1eaf2e6b43f8
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-21 15:49:15 +0530

    Create problem2.java

---
+++ problem2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    int arr[] = new int[100],n,i,j,temp;
+	    Scanner in = new Scanner(System.in);
+		n = in.nextInt();
+		for(i=0;i<n;i++)
+		{
+		    arr[i] = in.nextInt();
+		}
+		for(i =0;i<n;i++)
+		{
+		    if(arr[i] == arr[i+1])
+		        {
+		            arr[i] += arr[i+1];
+		            arr[i+1] = 0;
+		        }
+		}
+		for(i=0;i<n;i++)
+		{
+		    if(arr[i]==0)
+		    {
+		        for(j=i+1;j<n;j++)
+		        {
+		            if(arr[j]!=0)
+		            {
+		                temp = arr[j];
+		                arr[i] = arr[j];
+		                arr[j] = 0;
+		                break;
+		            }
+		        }
+		    }
+		}
+		for(i =0;i<n;i++)
+		{
+		    System.out.print(arr[i]+" ");
+		}
+	}	
+}
