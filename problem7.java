78197b3ade8f96497ff8896ec7f8b0d764fa51ab
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-22 13:19:17 +0530

    Create problem7.java

---
+++ problem7.java
@@ -0,0 +1,73 @@
+//input : {(())} output : True
+//input : } output : False
+public class Main 
+{
+    static int top =-1;
+    static void push(char x, char s[])
+    {
+        if(top == 99)
+        {
+            System.out.println("Stack is full");
+        }
+        else
+        {
+        s[++top] = x;
+        }
+        return;
+    }
+    static void pop()
+    {
+        top--;
+        return;
+    }
+    public static void main (String[] args) 
+    {
+        int i;
+        char s[] = new char[100];
+        char arr[] = {'}'};
+        for(i=0;i<arr.length;i++)
+        {
+            if(arr[i]=='{' || arr[i] == '(' || arr[i] =='[')
+            {
+                push(arr[i],s);
+            }
+            if(top == -1 && (arr[i] == '}' || arr[i] == ')' || arr[i] == ']'))
+            {
+                System.out.println("False");
+                System.exit(0);
+            }
+            else
+            {
+                if( arr[i]=='}')
+                {
+                    if(arr[top] == '{')
+                    {
+                        pop();
+                    }
+                }
+                else if(arr[i] == ')')
+                {
+                    if(arr[top] == '(')
+                    {
+                        pop();
+                    }
+                }
+                else
+                {
+                    if(arr[top] == '[')
+                    {
+                        pop();
+                    }
+                }
+            }
+        }
+        if(top==-1)
+        {
+            System.out.print("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
