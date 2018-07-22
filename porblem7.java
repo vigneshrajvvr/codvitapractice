234dda8f431621578600712b64096865fdb49123
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-22 12:56:55 +0530

    Create porblem7.java

---
+++ porblem7.java
@@ -0,0 +1,47 @@
+//Not yet completed
+public class Main 
+{
+    static int top =-1;
+    static void push(char x, char s[])
+    {
+        s[top++] = x;
+        return;
+    }
+    static void pop()
+    {
+        top--;
+        return;
+    }
+    public static void main (String[] args) 
+    {
+        int i;
+        char s[] = new char[100];
+        char arr[] = {'{','(','(',')',')','}'};
+        for(i=0;i<arr.length;i++)
+        {
+            if(arr[i]=='{' || arr[i] == '(' || arr[i] =='[')
+            {
+                push(arr[i],s);
+            }
+            else if( arr[i]=='}' || arr[i] == ')' || arr[i] == ']')
+            {
+                if(arr[top] == '{' || arr[top] == '(' || arr[top] == '[')
+                {
+                    pop();
+                }
+            }
+            else
+            {
+                pop();
+            }
+        }
+        if(top==-1)
+        {
+            System.out.print("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
