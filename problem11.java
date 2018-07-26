81b024d6070d7ae6dccb155ad28562e1e6457642
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-26 21:19:09 +0530

    Update problem11.java

--- problem11.java
+++ problem11.java
@@ -7,7 +7,7 @@ public class hourglass
         int arr[][] = {{1,2,3,4,5,6},{7,8,9,10,11,12},{13,14,15,16,17,18},{19,20,21,22,23,24},{25,26,27,28,29,30},{31,32,33,34,35,36}};
         int i,j;
         int n = arr.length, sum =0,max =0, i1 =0, i2=3, j1 =0 ,j2 =3;
-        while(i2 <= 5)
+        while(i2 <= 6)
         {
             for(i=i1;i<i2;i++)
             {
@@ -17,22 +17,23 @@ public class hourglass
                 }
             }
             System.out.println(""+j2);
-            System.out.println(""+sum);
-            if(j2<3) {
+            if(j2<=6) {
                 j1++;
                 j2++;
             }
-            if(j2==3)
+            if(j2>6)
             {
                 i1++;
                 i2++;
+                j1=0;
+                j2=3;
             }
             if(sum > max)
                 max = sum;
+            System.out.println(""+sum);
             sum =0;
-            j1=0;
-            j2=0;
         }
         System.out.println("Maximum hour glass sum : "+max);
     }
 }
+
