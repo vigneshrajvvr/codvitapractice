e9d27fbbcdbc7ea7a30946a31fbe6e1edd0c50a4
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-27 00:41:27 +0530

    Update problem11.java

--- problem11.java
+++ problem11.java
@@ -4,19 +4,21 @@ public class hourglass
 {
     public static void main(String argsp[])
     {
-        int arr[][] = {{1,2,3,4,5,6},{7,8,9,10,11,12},{13,14,15,16,17,18},{19,20,21,22,23,24},{25,26,27,28,29,30},{31,32,33,34,35,36}};
+        int arr[][] = {{1,1,1,0,0,0},{0,1,0,0,0,0},{1,1,1,0,0,0},{0,9,2,-4,-4,0},{0,0,0,-2,0,0},{0,0,-1,-2,-4,0}};
         int i,j;
-        int n = arr.length, sum =0,max =0, i1 =0, i2=3, j1 =0 ,j2 =3;
+        int sum =0, max =0, i1 =0, i2=3, j1 =0 ,j2 =3;
         while(i2 <= 6)
         {
             for(i=i1;i<i2;i++)
             {
                 for(j=j1;j<j2;j++)
                 {
-                    sum = sum + arr[i][j];
+                    if((j%2 == 0) && (i1 == i2-2))
+                        continue;
+                    else
+                        sum = sum + arr[i][j];
                 }
             }
-            System.out.println(""+j2);
             if(j2<=6) {
                 j1++;
                 j2++;
